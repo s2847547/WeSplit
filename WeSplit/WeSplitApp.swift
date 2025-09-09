9d148647bc696ed1d2b533ebd5ffc58a8d63c946
s2847547 <s2847547@students.fscj.edu>
2025-09-08 21:12:42 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Corey Burgos on 9/8/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
