4899ae2ca64a83bac281d0016a977ef3bf4fe98e
s2847547 <s2847547@students.fscj.edu>
2025-09-08 21:41:06 -0400

    Creating a form

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            Section {
+                Text("Hello, world!")
+            }
+
+            Section {
+                Text("Hello, world!")
+                Text("Hello, world!")
+            }
         }
-        .padding()
     }
 }
 
