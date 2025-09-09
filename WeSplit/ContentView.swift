19bb6a3294296ebdd8b4668dceebcae3081beada
s2847547 <s2847547@students.fscj.edu>
2025-09-09 18:41:00 -0400

    Modifying program state

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,30 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount = 0
+    
     var body: some View {
-        NavigationStack {
-            Form {
-                Section {
-                    Text("Hello, world!")
-                }
-                
-                Section {
-                    Text("Hello, world!")
-                    Text("Hello, world!")
-                    Text("Hello, world!")
-                }
-                
-                Section {
-                    Text("Hello, world!")
-                    Text("Hello, world!")
-                    Text("Hello, world!")
-                    Text("Hello, world!")
-                    Text("Hello, world!")
-                    Text("Hello, world!")
-                }
-            }
-            .navigationTitle("SwiftUI")
-            .navigationBarTitleDisplayMode(.inline)
+        Button("Tap Count: \(tapCount)") {
+            tapCount += 1
         }
     }
 }
