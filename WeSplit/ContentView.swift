4d55f141c8388c4ec849646b37cee9487d11245b
s2847547 <s2847547@students.fscj.edu>
2025-09-10 15:49:22 -0400

    Calculating the total per person

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,10 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var checkAmount = 0.0
-    @State private var numberOfPeople = 0
+    @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
     
     let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        // calculate the total per person
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+        
+    }
     
     var body: some View {
         NavigationStack {
@@ -47,7 +60,7 @@ struct ContentView: View {
                 
                 Section {
                     Text(
-                        checkAmount,
+                        totalPerPerson,
                         format: .currency(
                             code: Locale.current.currency?.identifier ?? "USD"
                         )
