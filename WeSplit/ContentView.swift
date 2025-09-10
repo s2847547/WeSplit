d11f2a3f5479ee65ae0d095dacc249f64bb3404a
s2847547 <s2847547@students.fscj.edu>
2025-09-09 20:20:07 -0400

    Creating pickers in a form

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,33 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var checkAmount = 0.0
-    @State private var numberOfPeople = 2
+    @State private var numberOfPeople = 0
     @State private var tipPercentage = 20
-
+    
     let tipPercentages = [10, 15, 20, 25, 0]
-
+    
     var body: some View {
-        Form {
-            Section {
-                TextField(
-                    "Amount",
-                    value: $checkAmount,
-                    format: .currency(
-                        code: Locale.current.currency?.identifier ?? "USD"
+        NavigationStack {
+            Form {
+                Section {
+                    TextField(
+                        "Amount",
+                        value: $checkAmount,
+                        format: .currency(
+                            code: Locale.current.currency?.identifier ?? "USD"
+                        )
                     )
-                )
-                .keyboardType(.decimalPad)
-
-            }
-
-            Section {
-                Text(
-                    checkAmount,
-                    format: .currency(
-                        code: Locale.current.currency?.identifier ?? "USD"
+                    .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                    
+                }
+                
+                Section {
+                    Text(
+                        checkAmount,
+                        format: .currency(
+                            code: Locale.current.currency?.identifier ?? "USD"
+                        )
                     )
-                )
+                }
             }
+            .navigationTitle("WeSplit")
         }
     }
 }
